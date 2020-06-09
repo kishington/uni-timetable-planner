5923d3e0d379f4b43632a36d4af7011688e43cef
Mihail <yarovmk@gmail.com>
2020-06-09 20:35:34 +0100

    Add SubjectDaoImpl

---
+++ src/main/java/ua/com/foxminded/university/spring/dao/impl/SubjectDaoImpl.java
@@ -0,0 +1,56 @@
+package ua.com.foxminded.university.spring.dao.impl;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import ua.com.foxminded.university.planner.Subject;
+import ua.com.foxminded.university.spring.dao.SubjectDao;
+import ua.com.foxminded.university.spring.dao.mappers.SubjectMapper;
+
+@Component
+public class SubjectDaoImpl implements SubjectDao {
+    
+    private JdbcTemplate jdbcTemplate;
+    
+    private final String SQL_GET_SUBJECT_BY_ID = "SELECT * FROM subjects WHERE subject_id = ?";
+    private final String SQL_GET_ALL = "SELECT * FROM subjects";
+    private final String SQL_DELETE_SUBJECT = "DELETE FROM subjects WHERE subject_id = ?";
+    private final String SQL_UPDATE_SUBJECT = "UPDATE subjects SET name = ? WHERE subject_id = ?";
+    private final String SQL_INSERT_SUBJECT = "INSERT INTO subjects(subject_id, name) values(?,?)";
+
+    @Autowired
+    public SubjectDaoImpl(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+    
+    @Override
+    public Subject getById(int subjectId) {
+        return jdbcTemplate.queryForObject(SQL_GET_SUBJECT_BY_ID, new Object[] {subjectId}, new SubjectMapper());
+    }
+
+    @Override
+    public List<Subject> getAll() {
+        return jdbcTemplate.query(SQL_GET_ALL, new SubjectMapper());
+    }
+
+    @Override
+    public boolean delete(Subject subject) {
+        return jdbcTemplate.update(SQL_DELETE_SUBJECT, subject.getId()) > 0;
+    }
+
+    @Override
+    public boolean update(Subject subject) {
+        return jdbcTemplate.update(SQL_UPDATE_SUBJECT, subject.getName(), subject.getId()) > 0;
+    }
+
+    @Override
+    public boolean create(Subject subject) {
+        return jdbcTemplate.update(SQL_INSERT_SUBJECT, subject.getId(), subject.getName()) > 0;
+    }
+
+}
---
+++ src/main/java/ua/com/foxminded/university/spring/dao/mappers/SubjectMapper.java
@@ -0,0 +1,20 @@
+package ua.com.foxminded.university.spring.dao.mappers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import ua.com.foxminded.university.planner.Subject;
+
+public class SubjectMapper implements RowMapper<Subject> {
+
+    @Override
+    public Subject mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Subject subject = new Subject();
+        subject.setId(resultSet.getInt("subject_id"));
+        subject.setName(resultSet.getString("name"));
+        return subject;
+    }
+
+}
